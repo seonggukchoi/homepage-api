300eb20509c3ec584f42b63b7100893ee16d29f1
seonggukchoi <seonggukchoi.kr@gmail.com>
2020-07-03 23:49:47 +0900

    Prettify code

--- src/modules/database/entities/index.ts
+++ src/modules/database/entities/index.ts
@@ -3,9 +3,4 @@ import { ProjectRoleEntity } from './project-role.entity';
 import { StackEntity } from './stack.entity';
 import { OrganizationEntity } from './organization.entity';
 
-export {
-  ProjectEntity,
-  ProjectRoleEntity,
-  StackEntity,
-  OrganizationEntity,
-};
+export { ProjectEntity, ProjectRoleEntity, StackEntity, OrganizationEntity };
